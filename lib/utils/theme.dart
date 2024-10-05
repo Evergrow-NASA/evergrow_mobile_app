cd7cb083f22ed343a5d216a53f06dd6a4fa7bec9
TMedalith <u20211f955@upc.edu.pe>
2024-10-05 08:30:57 -0500

    feat: Added desingn for start menu

--- lib/utils/theme.dart
+++ lib/utils/theme.dart
@@ -7,4 +7,10 @@ class AppTheme {
   static const Color frostColor = Colors.blueGrey;
   static const Color floodColor = Colors.lightBlueAccent;
 
+  static const Color neutral = Color(0xFF0C0C20);
+  static const Color primary1 = Color(0xFF52B69A);
+  static const Color primary2 = Color(0xFF447055);
+  static const Color secondary1 = Color(0xFFACC080);
+  static const Color secondary2 = Color(0xFFC4C4C4);
+  static const Color secondary3 = Color(0xFFDAE5DD);
 }
